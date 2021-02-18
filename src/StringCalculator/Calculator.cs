eb1d555e1c84f6e6fb2d36e090986ad74989bfd3
antao <joao.antao@gmail.com>
2021-02-18 22:38:53 +0100

    Add .NET5 solution and string calculator project with first step and respective unit tests

---
+++ src/StringCalculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace StringCalculator
+{
+    public sealed class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var array = numbers.Split(',');
+            
+            if (array.Length is 1)
+            {
+                return int.Parse(array[0]);
+            }
+
+            return int.Parse(array[0]) + int.Parse(array[1]);
+        }
+    }
+}
---
+++ tests/StringCalculator.UnitTests/CalculatorUnitTests.cs
@@ -0,0 +1,60 @@
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+namespace StringCalculator.UnitTests
+{
+    public class CalculatorUnitTests
+    {
+        private readonly Calculator _target;
+
+        public CalculatorUnitTests()
+        {
+            _target = new Calculator();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Add_Should_Return_Zero_When_String_Is_Null_Or_Empty(string expectedNumbers)
+        {
+            // Arrange
+            const int expected = 0;
+
+            // Act
+            var actual = _target.Add(expectedNumbers);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory, AutoData]
+        public void Add_Should_Return_Single_Number(int number)
+        {
+            // Arrange
+            var expected = number;
+            var numbers = $"{number}";
+
+            // Act
+            var actual = _target.Add(numbers);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory, AutoData]
+        public void Add_Should_Return_Sum(int number1, int number2)
+        {
+            // Arrange
+            var expected = number1 + number2;
+            var numbers = $"{number1},{number2}";
+
+            // Act
+            var actual = _target.Add(numbers);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
